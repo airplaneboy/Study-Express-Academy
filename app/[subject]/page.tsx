4ca5ad946bdfdf6de290dceb9ddc00f60b781adc
Agara Sulaiman <agarasualimany@gmail.com>
2023-07-21 21:32:57 +0100

    fixed bugs in subject route

--- app/[subject]/page.tsx
+++ app/[subject]/page.tsx
@@ -1,7 +1,5 @@
 import CardList from '@/containers/CardList';
 import { getSubject } from '@/lib/data/subjects';
-import getCourses from '@/lib/data/courses';
-import getUnits from '@/lib/data/units';
 
 const Subjects = async ({ params }: { params: { subject: string } }) => {
   const subject = await getSubject({ subjectId: params.subject });
--- containers/CardList.tsx
+++ containers/CardList.tsx
@@ -19,28 +19,27 @@ const CardList = ({
     <>
       {/* Header */}
       <CollapsibleHeader
-        initialHeight='h-28 text-6xl max-sm:text-3xl max-sm:h-24 max-md:text-center'
+        initialHeight='h-28 text-6xl max-sm:text-3xl max-sm:h-24 max-md:text-center '
         finalHeight='h-20 text-3xl text-center'
       >
-        <h1 className='max-w-7xl mx-auto w-full px-10 capitalize text-left font-bold font-inter inherit_text ease-linear transition-transform duration-300'>
+        <h1 className='no_wrap mx-auto w-full px-10 capitalize text-left font-bold font-inter inherit_text ease-linear transition-transform duration-300'>
           {contentHeader}
         </h1>
       </CollapsibleHeader>
 
-      <div className='flex w-full justify-between mx-auto max-w-7xl sm:px-10 py-16 bg-white p-4 md:rounded-b-3xl relative  md:top-32'>
+      <div className='flex mx-auto sm:px-10 py-16 bg-white p-4 md:top-32 relative'>
         {/* Sidebar */}
-        <aside>
-          <nav className='max-md:hidden h-[54rem] sticky top-44 lg:w-72 md:w-60 mr-4'>
-            <ul role='list' className=' h-max rounded-2xl border-gray-300 border p-5 '>
-              <h1 className='font-extrabold font-inter text-gray-700 mb-5  mr-3 text-3xl'>{sidebarHeader}</h1>
-              {sidebarArray.map((item) => (
+        <aside className='max-md:hidden'>
+          <nav className=' h-[45rem] sticky top-44 lg:w-80 md:w-72 overflow-y-auto rounded-2xl border-gray-300 border'>
+            <ul role='list' className='h-max p-5'>
+              <h1 className='font-extrabold font-inter text-gray-700 mb-5 mr-3 text-3xl'>{sidebarHeader}</h1>
+              {sidebarArray.map((sidebarItems) => (
                 <li
-                  key={item._id}
-                  className=' px-4 py-3 sm:px-0 text-md text-gray-500 hover:text-indigo-500 focus:text-indigo-600'
+                  key={sidebarItems._id}
+                  className=' px-4 py-3 sm:px-0 text-md text-gray-500 hover:text-indigo-500 focus:text-indigo-600 no_wrap'
                 >
-                  {/* <Link href={`#${item.title}`}>{item.title}</Link> */}
                   <Link
-                    to={item.title}
+                    to={sidebarItems.title}
                     spy={true}
                     smooth={true}
                     duration={300}
@@ -48,7 +47,7 @@ const CardList = ({
                     activeClass='active'
                     className='cursor-pointer'
                   >
-                    {item.title}
+                    {sidebarItems.title}
                   </Link>
                 </li>
               ))}
@@ -57,22 +56,28 @@ const CardList = ({
         </aside>
 
         {/* Content Header */}
-        <div className='flex-1  border-gray-300 lg:px-10 md:pl-10 rounded-r-xl'>
-          <ul role='list' className='divide-y divide-gray-200 '>
+        <div className='border-gray-300 lg:px-10 md:pl-10 w-full'>
+          <ul role='list'>
             {sidebarArray.map((headerItem) => (
-              <li key={headerItem._id} id={headerItem.title} className='px-4 pb-4 sm:px-0'>
-                <div className='bg-white overflow-hidden  border rounded-lg divide-y divide-gray-200'>
-                  <div className='px-4 py-5 sm:px-6 font-bold text-gray-700 flex items-center gap-2 text-lg'>
-                    {/* <Image width={30} height={30} src={item.image} alt='content images' /> */}
-                    <NextLink href={`/subjects/${headerItem.title}`}>{headerItem.title}</NextLink>
+              <li key={headerItem._id} id={headerItem.title} className='pb-4'>
+                <div className='bg-white border rounded-lg'>
+                  <div className='px-4 py-5 sm:px-6 font-bold text-gray-700 flex items-center gap-2 text-lg max-sm:text-base '>
+                    <NextLink className='max-md:truncate' href={`/${contentHeader}/${headerItem.title}`}>
+                      {headerItem.title}
+                    </NextLink>
                   </div>
 
                   {/* Content List */}
                   <div className='px-4 py-5 sm:p-6 bg-gray-50'>
-                    <ul className=' grid grid-cols-2 gap-2'>
+                    <ul className=' grid sm:grid-cols-2 gap-2'>
                       {headerItem.units.map((listItem) => (
-                        <li key={listItem._id} className='text-gray-600 text-md hover:underline'>
-                          <NextLink href={`/subjects/${listItem.title}`}>{listItem.title}</NextLink>
+                        <li
+                          key={listItem._id}
+                          className='text-gray-600 text-md max-sm:text-sm hover:underline truncate'
+                        >
+                          <NextLink href={`/${contentHeader}/${headerItem.title}/${listItem.title}`} className=''>
+                            {listItem.title}
+                          </NextLink>
                         </li>
                       ))}
                     </ul>
