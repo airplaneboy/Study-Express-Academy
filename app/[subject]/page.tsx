ef8f91577e8872016d570179fd4d7b035511b244
Agara Sulaiman <agarasualimany@gmail.com>
2023-07-23 07:46:00 +0100

    redirect to not-found if subject does not exist

--- app/[subject]/page.tsx
+++ app/[subject]/page.tsx
@@ -1,11 +1,15 @@
 import CardList from '@/containers/CardList';
 import { getSubject } from '@/lib/data/subjects';
 import lowercase from 'lodash.lowercase';
+import { notFound } from 'next/navigation';
 
 const Subjects = async ({ params }: { params: { subject: string } }) => {
-  const subject = await getSubject({ subjectId: lowercase(params.subject) });
-
-  return <CardList contentHeader={subject.title} sidebarArray={subject.courses} sidebarHeader='Courses' />;
+  try {
+    const subject = await getSubject({ subjectId: lowercase(params.subject) });
+    return <CardList contentHeader={subject.title} sidebarArray={subject.courses} sidebarHeader='Courses' />;
+  } catch (error) {
+    notFound();
+  }
 };
 
 export default Subjects;
