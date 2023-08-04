c3b2a90701c88777269d94de5bcb1befe45644ae
Agara Sulaiman <agarasualimany@gmail.com>
2023-08-04 01:35:54 +0100

    created methods to query dataset in sanity

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,1 +1,36 @@
 import { groq } from 'next-sanity';
+import client from './client-config';
+
+//#region Subjects
+export const getSubjects = async () =>
+  await client(groq`*[_type =='subjects']| order(title asc){title,description,courses[]->{title}}`);
+
+export const getSubject = async (title: string, slug?: string) =>
+  await client(groq`*[_type =='subjects'&&title==$title][0]{title,description,courses[]->{title}}`, { title });
+//#endregion
+
+//#region Courses
+export const getCourses = async () =>
+  await client(groq`*[_type =='courses']| order(title asc){title,description,units[]->{title}}`);
+
+export const getCourse = async (title: string, slug?: string) =>
+  await client(groq`*[_type =='courses'&&title==$title][0]{title,description,units[]->{title}}`, { title });
+//#endregion
+
+//#region Units
+export const getUnits = async () =>
+  await client(groq`*[_type =='units']| order(title asc){title,description,lessons[]->{title}}`);
+
+export const getUnit = async (title: string, slug?: string) =>
+  await client(groq`*[_type =='units'&&title==$title][0]{title,description,lessons[]->{title}}`, { title });
+//#endregion
+
+//#region Lessons
+export const getLessons = async () =>
+  await client(groq`*[_type =='lessons']| order(title asc){title,description,unit->{title},contents[]->{title}}`);
+
+export const getLesson = async (title: string, slug?: string) =>
+  await client(groq`*[_type =='lessons'&&title==$title][0]{title,description,unit->{title},contents[]->{title}}`, {
+    title,
+  });
+//#endregion
--- sanity/schemas/Subject.ts
+++ sanity/schemas/Subject.ts
@@ -10,13 +10,7 @@ const Subject = {
       description: 'Name of the subject. Hint: Biology',
       validation: (Rule: any) => Rule.required(),
     },
-    {
-      title: 'Subject Description',
-      name: 'description',
-      type: 'text',
-      description:
-        'What is the overview of this subject. Hint: Biology is the study of living organisms and their interactions with the environment, exploring topics like cell structure, evolution, reproduction, and genetics. It plays a crucial role in advancing our understanding of life and its applications in various fields...',
-    },
+
     {
       title: 'Subject Courses',
       name: 'courses',
@@ -24,6 +18,22 @@ const Subject = {
       of: [{ type: 'reference', to: [{ type: 'courses' }] }],
       description: 'The Courses that belong to this subject.',
       validation: (Rule: any) => Rule.unique(),
+    },
+    {
+      title: 'URL Path to Subject',
+      name: 'slug',
+      type: 'slug',
+      options: { source: 'title', maxLength: 300 },
+      description:
+        'The url path that leads to this subject. Hint: A slug of "advanced-math" would have the url: http://sitename/advanced-math. PS: You should auto-generate the slug unless you have a specific route in mind.',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      title: 'Subject Description',
+      name: 'description',
+      type: 'text',
+      description:
+        'What is the overview of this subject. Hint: Biology is the study of living organisms and their interactions with the environment, exploring topics like cell structure, evolution, reproduction, and genetics. It plays a crucial role in advancing our understanding of life and its applications in various fields...',
     },
   ],
 };
