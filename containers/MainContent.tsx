a73a97f390b1a6bed4f9e26b848faed435f00af3
Sulaiman Agara <airplaneboy00@gmail.com>
2024-01-11 23:17:24 -0500

    Made notification page responsive

--- containers/MainContent.tsx
+++ containers/MainContent.tsx
@@ -6,7 +6,7 @@ const MainContent = ({ children }: { children: any }) => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <main className='flex-1'>
+    <main className='flex-1 min-h-[calc(100vh-56px-56px-82px-114px)] sm:min-h-[calc(100vh-64px-56px-50px-114px)] md:min-h-[calc(100vh-64px-50px-114px)]'>
       <div className='py-6 px-2'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8 sm:mb-10'>
           <span className='max-sm:text-4xl sm:text-5xl font-extrabold text-gray-800  max-sm:text-center capitalize'>
