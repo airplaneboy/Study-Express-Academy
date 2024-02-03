9534ac508c06b17e38a88b2215abef7cc901e559
Sulaiman Agara <airplaneboy00@gmail.com>
2024-02-03 16:49:37 -0500

    check if there's a test before display quiz

--- containers/CardList.tsx
+++ containers/CardList.tsx
@@ -117,7 +117,7 @@ const CardList = ({
                             <div className='absolute h-full w-full background-stripe mix-blend-multiply pointer-events-none' />
                             <div
                               className={
-                                'overflow-hidden font-black shadow-yellow-600 shadow-sm  bg-gradient-to-r from-yellow-400  via-yellow-200 to-yellow-400 px-3 py-5 block border-yellow-600 border rounded-2xl '
+                                'overflow-hidden font-black shadow-yellow-600 shadow-inner  bg-gradient-to-r from-yellow-400  via-yellow-200 to-yellow-400 px-3 py-5 block border-yellow-600 border rounded-2xl '
                               }>
                               <div className='px-4 py-3 sm:px-6 flex items-center gap-2 '>
                                 {headerItem?.image && (
@@ -239,43 +239,45 @@ const CardList = ({
                                   })}
                               </ul>
                               {/* ====================================Cut Here==================================== */}
-                              <div className='pl-10'>
-                                <span className='text-sm font-light mb-5 block text-gray-400'>Quizzes</span>
-                                <ul className='gap-2 flex flex-col'>
-                                  {contentArray &&
-                                    (headerItem as any)[contentArray]?.map((listItem: any) => {
-                                      if (listItem.slug.endsWith('test'))
-                                        return (
-                                          <li key={listItem?._id}>
-                                            <CustomLink
-                                              className={cx(
-                                                completedContents?.some((item) => item == listItem?._id)
-                                                  ? 'bg-purple-100 text-purple-600 border-purple-600 hover:bg-purple-200'
-                                                  : 'bg-neutral-100 text-neutral-600 border-neutral-600 hover:bg-neutral-200',
-                                                'border-r-8 my-[1px] ml-[1px] font-semibold block p-3 hover:my-0 hover:ml-0 hover:border-y hover:border-l rounded-lg  text-base word-spacing-1 tracking-wide '
-                                              )}
-                                              pathStrings={[slug, headerItem?.slug, listItem?.slug]}>
-                                              <div className='flex flex-col'>
-                                                {listItem?.title}
-                                                <span className='text-sm text-gray-400 font-normal slashed-zero'>
-                                                  {listItem?.numberOfQuestions || 0} Questions
-                                                </span>
-                                                <span
-                                                  className={cx(
-                                                    completedContents?.some((item) => item == listItem?._id)
-                                                      ? ''
-                                                      : 'text-transparent',
-                                                    'text-xs text-gray-500 font-normal mt-2 '
-                                                  )}>
-                                                  Completed
-                                                </span>
-                                              </div>
-                                            </CustomLink>
-                                          </li>
-                                        );
-                                    })}
-                                </ul>
-                              </div>
+                              {(headerItem as any)[contentArray].some((item: any) => item.slug.endsWith('test')) && (
+                                <div className='pl-10'>
+                                  <span className='text-sm font-light mb-5 block text-gray-400'>Quizzes</span>
+                                  <ul className='gap-2 flex flex-col'>
+                                    {contentArray &&
+                                      (headerItem as any)[contentArray]?.map((listItem: any) => {
+                                        if (listItem.slug.endsWith('test'))
+                                          return (
+                                            <li key={listItem?._id}>
+                                              <CustomLink
+                                                className={cx(
+                                                  completedContents?.some((item) => item == listItem?._id)
+                                                    ? 'bg-purple-100 text-purple-600 border-purple-600 hover:bg-purple-200'
+                                                    : 'bg-neutral-100 text-neutral-600 border-neutral-600 hover:bg-neutral-200',
+                                                  'border-r-8 my-[1px] ml-[1px] font-semibold block p-3 hover:my-0 hover:ml-0 hover:border-y hover:border-l rounded-lg  text-base word-spacing-1 tracking-wide '
+                                                )}
+                                                pathStrings={[slug, headerItem?.slug, listItem?.slug]}>
+                                                <div className='flex flex-col'>
+                                                  {listItem?.title}
+                                                  <span className='text-sm text-gray-400 font-normal slashed-zero'>
+                                                    {listItem?.numberOfQuestions || 0} Questions
+                                                  </span>
+                                                  <span
+                                                    className={cx(
+                                                      completedContents?.some((item) => item == listItem?._id)
+                                                        ? ''
+                                                        : 'text-transparent',
+                                                      'text-xs text-gray-500 font-normal mt-2 '
+                                                    )}>
+                                                    Completed
+                                                  </span>
+                                                </div>
+                                              </CustomLink>
+                                            </li>
+                                          );
+                                      })}
+                                  </ul>
+                                </div>
+                              )}
                             </div>
                           </div>
                         )}
