b99d3cb3910ef3410c25bde1ddfb746821eba801
Sulaiman Agara <airplaneboy00@gmail.com>
2024-02-05 01:12:16 -0500

    fixed minor bug with content description

--- containers/CardListContentDescription.tsx
+++ containers/CardListContentDescription.tsx
@@ -2,7 +2,7 @@ const CardListContentDescription = ({ contentDescription }: { contentDescription
   return (
     <>
       {contentDescription && (
-        <div className='w-full mb-10 flex flex-col gap-5 border-2 border-gray-700 text-gray-500 sm:bg-gray-200 sm:rounded-2xl mx-4 max-sm:px-2 max-sm:py-2 max-sm:pb-2 sm:p-4'>
+        <div className='w-full mb-10 flex flex-col gap-5 max-sm:border-x-0 border-2 border-gray-700 text-gray-500 bg-gray-200 sm:rounded-2xl mx-4 max-sm:px-2 max-sm:py-2 max-sm:pb-2 sm:p-4'>
           <span className=' text-gray-600 font-extrabold underline-offset-8  underline decoration-purple-500'>
             Here<span className='text-purple-500 text-lg'>&apos;</span>s What you should know!
           </span>
