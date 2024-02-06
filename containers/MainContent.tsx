afdc8bff489409b8bc4bf50c14def87e4e6b0a7c
Sulaiman Agara <airplaneboy00@gmail.com>
2024-02-05 22:52:23 -0500

    Added attribution for lordicon

--- containers/MainContent.tsx
+++ containers/MainContent.tsx
@@ -6,7 +6,7 @@ const MainContent = ({ children }: { children: any }) => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <main className='flex-1 min-h-[calc(100vh-56px-56px-82px)] sm:min-h-[calc(100vh-64px-56px-50px)] md:min-h-[calc(100vh-64px-50px)]'>
+    <main className='flex-1 min-h-[calc(100vh-56px-56px-116.5px)] sm:min-h-[calc(100vh-64px-56px-50px)] md:min-h-[calc(100vh-64px-50px)]'>
       <div className='py-6 pb-28 px-2'>
         <div className='max-w-7xl mx-auto px-2 sm:px-6 md:px-8 sm:mb-10'>
           <span className='max-sm:text-4xl sm:text-5xl font-extrabold text-gray-800  max-sm:text-center capitalize'>
