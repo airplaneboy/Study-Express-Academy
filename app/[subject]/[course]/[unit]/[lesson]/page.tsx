58d2cd14582df2829576195c6e0d8971a5613111
Sulaiman Agara <airplaneboy00@gmail.com>
2024-02-09 23:07:32 -0500

    Added bar to close and open sidebar

--- app/[subject]/[course]/[unit]/[lesson]/page.tsx
+++ app/[subject]/[course]/[unit]/[lesson]/page.tsx
@@ -1,6 +1,6 @@
 const Lesson = () => {
   return (
-    <div className='px-2 sm:px-8 lg:px-16 py-4 sm:py-8 md:py-4 md:text-center lg:text-left flex flex-col justify-evenly sm:justify-center lg:justify-evenly h-full tracking-tight'>
+    <div className='px-2 sm:px-8 lg:px-16 py-4 sm:py-8 md:py-4 md:text-center lg:text-left flex flex-col justify-evenly sm:justify-center lg:justify-evenly tracking-tight h-full'>
       <span className='border-l-4 pl-2 border-blue-500 font-extrabold text-xs sm:text-base md:text-lg text-gray-600'>
         Welcome to the Lessons Section.
       </span>
@@ -9,6 +9,8 @@ const Lesson = () => {
         <span className='text-blue-500'>&apos;</span>re interested in on the{' '}
         <span className='text-blue-500'>sidebar</span>.
       </span>
+
+      {/* Next Button Cover */}
       <div className=' block h-[65px] sm:h-16 bg-white border-dashed sm:border-solid border-t-2 sm:border-t absolute sm:fixed md:absolute bottom-0 left-0 right-0 max-md:-translate-x-1/2 max-md:left-1/2 min-w-full' />
     </div>
   );
