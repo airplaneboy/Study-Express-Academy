a8eebbce54e5050cf200bd6826b659782f90d14a
Sulaiman Agara <airplaneboy00@gmail.com>
2024-03-25 23:21:44 -0400

    Added graph data

--- app/user/tests-overview/[slug]/page.tsx
+++ app/user/tests-overview/[slug]/page.tsx
@@ -7,9 +7,12 @@ const TestOverview = async ({ params }: { params: { slug: string } }) => {
 
   const test = user.contentProgress.tests.find((item: any) => item.slug == params.slug);
 
+  const subItem: string | undefined = 'font-bold text-gray-800 text-sm';
+  const item: string | undefined = 'flex justify-between py-2';
   return (
     <div>
       <LineChart
+        title={`${test?.testTitle} Score Trend`}
         data={{
           labels: test.scores.map((score: any) => formatDistanceToNow(new Date(score.date))),
           datasets: [
@@ -18,11 +21,36 @@ const TestOverview = async ({ params }: { params: { slug: string } }) => {
               label: 'Score Average',
               backgroundColor: '#3b82f6',
               borderColor: '#60a5fa',
-              interpolate: true,
+              cubicInterpolationMode: 'monotone',
+              tension: 0.4,
             },
           ],
         }}
       />
+
+      <h2 className='mt-16 mb-2 text-center uppercase text-xl tracking-wider text-gray-800 font-extrabold'>
+        More Information
+      </h2>
+      <ul className='flex flex-col text-gray-600 text-base divide-y'>
+        <li className={item}>
+          <span className={subItem}>Number of Times Taken: </span>
+          {test?.numberOfTimesTaken}
+        </li>
+        <li className={item}>
+          <span className={subItem}>Number of Times Passed: </span>
+          {test?.numberOfTimesPassed}
+        </li>
+        <li className={item}>
+          <span className={subItem}>Number of Questions: </span>
+          {test?.numberOfQuestions}
+        </li>
+        <li className={item}>
+          <span className={subItem}>Last test taken: </span>
+          {formatDistanceToNow(test?.lastTaken, { addSuffix: true, includeSeconds: true })}
+        </li>
+      </ul>
+
+      {/* <pre>{JSON.stringify(test, null, 2)}</pre> */}
     </div>
   );
 };
--- components/Charts/Line.tsx
+++ components/Charts/Line.tsx
@@ -9,57 +9,105 @@ import {
   Point,
   ChartData,
   CategoryScale,
-  Legend,
+  // Legend,
   Tooltip,
-  Interaction,
 } from 'chart.js';
-import { CrosshairPlugin, Interpolate } from 'chartjs-plugin-crosshair';
+import('chartjs-plugin-zoom').then((plugin) => ChartJS.register(plugin.default));
+import { ForwardedRef, useEffect, useRef, useState } from 'react';
+import { ChartJSOrUndefined } from 'react-chartjs-2/dist/types';
 
-ChartJS.register(LineElement, PointElement, LinearScale, Title, CategoryScale, Legend, Tooltip, CrosshairPlugin);
-Interaction.modes.interpolate = Interpolate;
+ChartJS.register(LineElement, PointElement, LinearScale, Title, CategoryScale, Tooltip);
 
-export default function LineChart({ data }: { data: ChartData<'line', (number | Point | null)[], unknown> }) {
+export default function LineChart({
+  data,
+  title,
+}: {
+  title?: string;
+  data: ChartData<'line', (number | Point | null)[], unknown>;
+}) {
+  const chartRef: ForwardedRef<ChartJSOrUndefined<'line', (number | Point | null)[], unknown>> | undefined = useRef();
+  const [isZoomed, setIsZoomed] = useState(chartRef.current?.isZoomedOrPanned());
+
+  useEffect(() => {
+    import('chartjs-plugin-zoom').then((plugin) => ChartJS.register(plugin.default));
+    chartRef.current?.zoom(1.5);
+  }, []);
+
   return (
-    <Line
-      data={data}
-      options={{
-        // animation: false,
-        plugins: {
-          tooltip: {
-            mode: 'interpolate',
-            intersect: false,
+    <div className='relative'>
+      <Line
+        ref={chartRef}
+        data={data}
+        options={{
+          font: {
+            weight: 400,
+            family:
+              "'__Plus_Jakarta_Sans_e3c363', '__Plus_Jakarta_Sans_Fallback_e3c363','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
           },
-          crosshair: {
-            line: {
-              color: '#F66', // crosshair line color
-              width: 1, // crosshair line width
-            },
-            sync: {
-              enabled: true, // enable trace line syncing with other charts
-              group: 1, // chart group
-              suppressTooltips: false, // suppress tooltips when showing a synced tracer
-            },
+          scales: {
+            y: { min: 0, max: 1 },
+          },
+          // animation: false,
+          plugins: {
             zoom: {
-              enabled: true, // enable zooming
-              zoomboxBackgroundColor: 'rgba(66,133,244,0.2)', // background color of zoom box
-              zoomboxBorderColor: '#48F', // border color of zoom box
-              zoomButtonText: 'Reset Zoom', // reset zoom button text
-              zoomButtonClass: 'reset-zoom', // reset zoom button class
+              zoom: {
+                wheel: {
+                  enabled: true,
+                },
+                pinch: {
+                  enabled: true,
+                },
+                drag: {
+                  enabled: true,
+                  modifierKey: 'shift',
+                },
+                mode: 'x',
+                // onZoomStart: () => setIsZoomed(chartRef.current?.isZoomedOrPanned),
+                // onZoomComplete: (e) => setIsZoomed(e.chart.isZoomedOrPanned()),
+              },
+              pan: {
+                enabled: true,
+              },
+              limits: {
+                y: {
+                  min: 0,
+                  max: 1,
+                },
+              },
             },
-            callbacks: {
-              beforeZoom: () =>
-                function (start, end) {
-                  // called before zoom, return false to prevent zoom
-                  return true;
-                },
-              afterZoom: () =>
-                function (start, end) {
-                  // called after zoom
-                },
+            tooltip: {
+              intersect: false,
+              padding: 12,
+              bodyFont: {
+                family:
+                  "'__Plus_Jakarta_Sans_e3c363', '__Plus_Jakarta_Sans_Fallback_e3c363','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+              },
+              titleFont: {
+                family:
+                  "'__Plus_Jakarta_Sans_e3c363', '__Plus_Jakarta_Sans_Fallback_e3c363','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+              },
+            },
+            title: {
+              display: true,
+              text: title,
+              padding: { bottom: 30 },
+              font: {
+                // size: 14,
+                weight: 400,
+                family:
+                  "'__Plus_Jakarta_Sans_e3c363', '__Plus_Jakarta_Sans_Fallback_e3c363','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+              },
             },
           },
-        },
-      }}
-    />
+        }}
+      />
+      {/* {isZoomed && ( */}
+      <button
+        onClick={() => chartRef.current?.resetZoom()}
+        className='absolute left-[10%] top-[3%] py-1 px-2 rounded-lg bg-black text-white text-xs opacity-30 hover:opacity-70 transition-opacity'>
+        Reset
+      </button>
+      {/* )} */}
+    </div>
   );
 }
