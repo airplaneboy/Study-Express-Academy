7223099bd390d2f731f9ec116fed0efb09a70f37
Sulaiman Agara <airplaneboy00@gmail.com>
2024-04-14 17:57:41 -0400

    Feature Section styled

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,31 +1,9 @@
-import { getQuestion } from '@/sanity/sanity-utils';
-import TeX from '@/components/TeX';
-import TeXExtractor from '@/containers/TeXExtractor';
+import FeatureSection from '@/containers/Landing Page/FeatureSection';
 
 const Test = async () => {
-  const question: { options: any[] } = await getQuestion('b2e0b5fc-2b9e-4938-b815-61923dd740ac');
-  question.options.push('3$', '3 $ 5\\$ $ ');
-
   return (
     <div className='h-[200vh] flex flex-col gap-1'>
-      {question.options.map((option: string, index) => {
-        return (
-          <span key={index}>
-            {/* {option
-              .split(/\$\$(.*?)\$\$/g)
-              .map((match, index) =>
-                index % 2 === 0 ? (
-                  match
-                    .split(/\$ (.*?) \$/g)
-                    .map((match, index) => (index % 2 === 0 ? match : <TeX key={index}>{match}</TeX>))
-                ) : (
-                  <TeX key={index}>{match}</TeX>
-                )
-              )} */}
-            <TeXExtractor value={option} />
-          </span>
-        );
-      })}
+      <FeatureSection />
     </div>
   );
 };
--- containers/Landing Page/FeatureSection.tsx
+++ containers/Landing Page/FeatureSection.tsx
@@ -1,19 +1,21 @@
 const FeatureSection = () => {
   return (
-    <section className={'max-w-7xl mx-auto flex flex-row justify-between items-center py-32 px-24 gap-20'}>
+    <section
+      className={
+        'bg-[radial-gradient(circle,_rgba(191,219,254,1)_0%,_rgba(221,214,254,1)_15%,_rgba(255,241,242,1)_30%,_rgba(255,255,255,1)_50%)] max-w-7xl mx-auto flex flex-row justify-between items-center py-32 px-24 gap-20'
+      }>
       <div className='w-full h-full flex flex-col items-center justify-center gap-10'>
-        <h1 className='text-5xl leading-[1] font-extrabold'>
-          Transform your learning experience with our interactive courses
+        <h1 className='text-5xl leading-[1] font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-red-400 tracking-tight'>
+          Transform your learning with our interactive courses
         </h1>
-        <p className='leading-8 text-lg font-medium text-gray-800'>
-          We believe that knowledge is power. Our courses are designed to stir your curiosity and fire up your
-          imagination. From math and science to arts and philosophy, journey through an array of subjects like never
-          before!
+        <p className='leading-8 text-lg font-semibold text-gray-500 '>
+          We believe that knowledge is power, that&apos;s why our courses are designed to engage your imagination. From
+          math and science to arts and language, we&apos;ll take you through an array of subjects like never before!
         </p>
       </div>
 
-      <div className='w-full h-full flex flex-col justify-center items-center gap-10'>
-        <div className='flex gap-5 items-center justify-between'>
+      <div className='w-full h-full flex flex-col justify-center items-center gap-10 min-w-[496px] border-0 py-5'>
+        {/* <div className='flex gap-5 items-center justify-between'>
           <div className='flex flex-col gap-3'>
             <span className=' font-bold text-xl tracking-wide'>Personalized Learning Analytics</span>
             <p className='text-sm  leading-6'>
@@ -33,7 +35,18 @@ const FeatureSection = () => {
           </div>
 
           <div className='w-full h-32 bg-red-500' />
-        </div>
+        </div> */}
+        <video
+          className='shadow-lg border-t shadow-gray-400 rounded-2xl w-full mr-[2px] border-0 overflow-hidden'
+          loop
+          autoPlay
+          muted
+          width='720'
+          height='720'
+          preload='auto'>
+          <source src='/assets/Subjects.webm' type='video/webm' />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </section>
   );
