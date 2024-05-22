ecb3a2068916d3ec2bd2e9f00b2d6594a2a376a8
Sulaiman Agara <airplaneboy00@gmail.com>
2024-05-21 23:49:22 -0400

    Made footer section responsive

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,19 +7,21 @@ import ParallaxSection from '@/components/ParallaxSection';
 import IntersectionVideo from '@/containers/Landing Page/IntersectionVideoComponent';
 import HeroSection from '@/containers/Landing Page/HeroSection';
 import FeatureSection from '@/containers/Landing Page/FeatureSection';
+import FooterSection from '@/containers/Landing Page/FooterSection';
 // import dynamic from 'next/dynamic';
 // const TestimonialSection = dynamic(() => import('@/containers/Landing Page/TestimonialSection'), { ssr: false });
 const Test = async () => {
   return (
     <div className='h-[200vh] pt-[100vh] w-full'>
       {/* <BenefitSection /> */}
-      <CTASection />
+      {/* <CTASection /> */}
       {/* <TestimonialSection /> */}
       {/* <ParallaxSection /> */}
       {/* <IntersectionVideo src='/parallax/video.webm' /> */}
       {/* <HeroSection /> */}
       {/* <FeatureSection /> */}
       {/* <FeatureSection2 /> */}
+      <FooterSection />
     </div>
   );
 };
--- containers/Landing Page/FooterSection.tsx
+++ containers/Landing Page/FooterSection.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link';
 
 const FooterSection = () => {
-  const column: string | undefined = 'flex gap-5 flex-col ';
+  const column: string | undefined = 'inline-flex gap-5 flex-col max-md:w-full';
   const columnHeader = 'font-bold text-sm text-gray-800';
   const columnList = 'text-xs flex flex-col gap-3';
 
   return (
-    <section className='max-w-7xl mx-auto flex flex-row justify-between items-center min-h-[50vh] px-24 py-12 gap-10'>
-      <div className=' flex-1'>
+    <section className='max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center min-h-[50vh] px-6 md:px-12 lg:px-24 py-12 gap-10'>
+      <div className=' flex-1 max-lg:text-center'>
         <h1 className='pb-5 w-full text-4xl tracking-tight font-extrabold bg-gradient-to-r from-red-500 via-purple-500 to-blue-500 bg-clip-text text-transparent'>
           Providing personalized education for every learner&apos;s journey.
         </h1>
       </div>
-      <div className='columns-3 flex flex-wrap gap-10'>
+      <div className='columns-2 max-md:space-y-10 justify-between md:columns-3 grid-cols-2 md:flex md:flex-wrap gap-10'>
         <div className={column}>
           <span className={columnHeader}>About</span>
           <ul className={columnList}>
@@ -26,10 +26,16 @@ const FooterSection = () => {
           <span className={columnHeader}>Support and Help</span>
           <ul className={columnList}>
             <li>
-              <Link href='#'>Contact Us Help</Link>
+              <Link href='#'>Contact Us</Link>
             </li>
             <li>
-              <Link href='#'>Support Feedback</Link>
+              <Link href='#'>Help</Link>
+            </li>
+            <li>
+              <Link href='#'>Feedback</Link>
+            </li>
+            <li>
+              <Link href='#'>Support</Link>
             </li>
             <li>
               <Link href='#'>FAQs</Link>
